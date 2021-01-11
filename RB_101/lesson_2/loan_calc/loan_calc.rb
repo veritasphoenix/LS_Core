82ee4b47d6a4ffaef47e393ee3890b162081ab1a
Dan Hope <dan@ckrit.info>
2021-01-11 14:31:52 -0800

    updated loan calc after reading solution

--- RB_101/lesson_2/loan_calc/loan_calc.rb
+++ RB_101/lesson_2/loan_calc/loan_calc.rb
@@ -1,71 +1,3 @@
-# loan_calc.rb
-
-# monthly interest rate
-# loan duration in months
-# monthly payment
-
-# ask the user for the interest rate
-# -ask the user if that is yearly or monthly
-# - Check if interest rate is a number
-# if it is yearly, convert APR to months
-# -ask the user for the loan duration
-# Check if loan duration is a number
-# -ask the user if that is months or years
-# -if it is yearly, convert to months
-# ask the user for the loan amount
-# calculate the monthy payment
-# tell the user the monthy payment
-# ask the user if s/he wants to do another calculation
-
-# Move all prompts to external .yml file
-# import .yml file -
-# translate all prompts to spanish
-# create a prompt method that takes in prompt and appands '=>' to the beginning
-# 
-
-# START
-
-# GET loan_amount - float
-# SUBPROCESS check if loan_ampunt is a number
-# => GET number from main program
-# => IF number, return true
-# => IF not, return false
-
-# GET interest_rate APR - float
-# SUBPROCESS check IF interest_rate is a number
-# => GET number from main program
-# => IF number, return true
-# => IF not, return false
-
-# GET interst_rate_timing for monthly or yearly interest rate
-# SUBPROCESS check if interest_rate_timing is a number
-# => GET number from main program
-# => IF number, return true
-# => IF not, return false
-# IF yearly, divide by 12 months
-
-# GET loan_duration - integer
-# SUBPROCESS check IF loan_duratin is a number
-# => GET number from main program
-# => IF number, return true
-# => IF not, return false
-
-# GET loan_duration_timing in years or months
-# SUBPROCESS check if loan_duration_timing is a number
-# => GET number from main program
-# => IF number, return true
-# => IF not, return false
-# => IF yearly, divide loan_duration by 12 months
-
-# monthly_payment = loan_amount * (interest_rate / (1 - (1 + interest_rate)**(-loan_duration)))
-
-# PRINT monthly_payment
-# PRINT message asking if they want another calculation
-# IF yes, loop back to begining
-# IF no, PRINT a thank you and exit program
-
-# END
-require 'pry'
 require 'yaml'
 PROMPT_FILE = YAML.load_file('prompts.yml')
 
@@ -114,11 +46,11 @@ loop do
 end
 
 loop do
-  loan_amount = ''  
+  loan_amount = ''
   loop do
     puts prompt 'enter_amount', lang
     loan_amount = gets.chomp
-    if float?(loan_amount)
+    if float?(loan_amount) && loan_amount.to_f > 0
       loan_amount = loan_amount.to_f
       break
     else
@@ -130,27 +62,25 @@ loop do
   loop do
     puts prompt 'enter_interest_rate', lang
     interest_rate = gets.chomp
-    if float?(interest_rate)
-      interest_rate = interest_rate.to_f / 100 # convert whole num percent to decimal
+    if float?(interest_rate) && interest_rate.to_f > 0
+      # convert whole num percent to decimal
+      interest_rate = interest_rate.to_f / 100
       break
     else
       puts prompt 'invalid_number', lang
     end
   end
 
-  interest_rate_timing = ''
   loop do
     puts prompt 'enter_interest_rate_timing', lang
     interest_rate_timing = gets.chomp.downcase
     if ['y', 'a', 'm'].include?(interest_rate_timing)
       if interest_rate_timing == 'm'
-        interest_rate = interest_rate / 12
-        break
-      else
-        break
+        interest_rate /= 12
       end
+      break
     else
-      puts prompt 'enter_interest_rate_timing_error', lang
+      puts prompt 'invalid_input', lang
     end
   end
 
@@ -158,7 +88,7 @@ loop do
   loop do
     puts prompt 'enter_loan_duration', lang
     loan_duration = gets.chomp
-    if integer?(loan_duration)
+    if integer?(loan_duration) && loan_duration.to_i > 0
       loan_duration = loan_duration.to_i
       break
     else
@@ -166,13 +96,12 @@ loop do
     end
   end
 
-  loan_duration_timing = ''
   loop do
     puts prompt 'enter_loan_duration_timing', lang
     loan_duration_timing = gets.chomp.downcase
     if ['m', 'y', 'a'].include?(loan_duration_timing)
       if loan_duration_timing == 'y' || loan_duration_timing == 'a'
-        loan_duration = loan_duration * 12
+        loan_duration *= 12
       end
       break
     else
@@ -180,23 +109,83 @@ loop do
     end
   end
 
-  monthly_payment = (loan_amount * (interest_rate / (1 - (1 + interest_rate)**(-loan_duration)))).round(2)
+  monthly_payment =
+    loan_amount * (interest_rate / (1 - (1 + interest_rate)**(-loan_duration)))
 
-  puts "#{prompt('monthly_payment_amount', lang)} $#{monthly_payment}"
+  puts "#{prompt('monthly_payment_amount', lang)} $" +
+       format('%.2f', monthly_payment).to_s
 
-  run_again = ''
-  loop do
-    puts prompt 'another_calculation', lang
-    run_again = gets.chomp.downcase
-
-    if ['y', 'n'].include?(run_again)
-      if run_again == 'n'
-        break
-      else
-        break
-      end
-    else
-      puts prompt 'invalid_input', lang
-    end
-  end
+  puts prompt 'another_calculation', lang
+  run_again = gets.chomp.downcase
+  break unless run_again.start_with?('y') || run_again.start_with?('s')
 end
+
+puts "Thank you, #{users_name}, for using the Mortgage Calculator!"
+puts "Good bye!"
+
+# loan_calc.rb
+
+# monthly interest rate
+# loan duration in months
+# monthly payment
+
+# ask the user for the interest rate
+# -ask the user if that is yearly or monthly
+# - Check if interest rate is a number
+# if it is yearly, convert APR to months
+# -ask the user for the loan duration
+# Check if loan duration is a number
+# -ask the user if that is months or years
+# -if it is yearly, convert to months
+# ask the user for the loan amount
+# calculate the monthy payment
+# tell the user the monthy payment
+# ask the user if s/he wants to do another calculation
+
+# Move all prompts to external .yml file
+# import .yml file -
+# translate all prompts to spanish
+# create a prompt method that takes in prompt and appands '=>' to the beginning
+
+# START
+
+# GET loan_amount - float
+# SUBPROCESS check if loan_ampunt is a number
+# => GET number from main program
+# => IF number, return true
+# => IF not, return false
+
+# GET interest_rate APR - float
+# SUBPROCESS check IF interest_rate is a number
+# => GET number from main program
+# => IF number, return true
+# => IF not, return false
+
+# GET interst_rate_timing for monthly or yearly interest rate
+# SUBPROCESS check if interest_rate_timing is a number
+# => GET number from main program
+# => IF number, return true
+# => IF not, return false
+# IF yearly, divide by 12 months
+
+# GET loan_duration - integer
+# SUBPROCESS check IF loan_duratin is a number
+# => GET number from main program
+# => IF number, return true
+# => IF not, return false
+
+# GET loan_duration_timing in years or months
+# SUBPROCESS check if loan_duration_timing is a number
+# => GET number from main program
+# => IF number, return true
+# => IF not, return false
+# => IF yearly, divide loan_duration by 12 months
+
+# monthly_payment = loan_amount * (interest_rate / (1 - (1 + interest_rate)**(-loan_duration)))
+
+# PRINT monthly_payment
+# PRINT message asking if they want another calculation
+# IF yes, loop back to begining
+# IF no, PRINT a thank you and exit program
+
+# END
