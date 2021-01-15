aba389de138884d2562faab385f3e60673f78082
Dan Hope <dan@ckrit.info>
2021-01-15 12:25:57 -0800

    loac calc 2nd refactor

--- RB_101/lesson_2/loan_calc/loan_calc_refactored.rb
+++ RB_101/lesson_2/loan_calc/loan_calc_refactored.rb
@@ -34,11 +34,6 @@ end
 def clear_screen
   system('clear') || system('cls')
 end
-clear_screen
-
-# PROMPT_FILE is referenced directly prior to determining desired output
-# language
-puts "=> #{PROMPT_FILE['welcome']}"
 
 # Get users desired display language, and save it
 # to the global variable $lang
@@ -57,147 +52,151 @@ def get_lang
     end
   end
 end
-get_lang
 
 # Get the users name for use in various output
 def get_users_name
-  puts prompt 'enter_users_name'
-  user_name = gets.chomp.strip
-  if user_name.empty?
-    puts prompt 'invalid_input_name'
-  else
-    puts format(prompt('hello'), name: user_name)
+  user_name = ''
+  
+  loop do
+    puts prompt 'enter_users_name'
+    user_name = gets.chomp.strip
+    if user_name.empty?
+      puts prompt 'invalid_input_name'
+    else
+      puts format(prompt('hello'), name: user_name)
+      break
+    end
   end
+
   user_name
 end
-users_name = get_users_name
 
-# Main logic loop
-loop do
-  # Get the amount of the loan
-  def get_loan_amount
-    loan_amount = ''
-    loop do
-      puts prompt 'enter_amount'
-      loan_amount = gets.chomp.strip
-      if valid_number_entry? loan_amount
-        loan_amount = loan_amount.to_f
-        break
-      else
-        puts prompt 'invalid_number'
-      end
-    end
-    loan_amount
-  end
-  loan_amount = get_loan_amount
+# Get the amount of the loan
+def get_loan_amount
+  loan_amount = ''
 
-  # Get whether interest rate is monthly or yearly, and if it is monthly,
-  # convert it to yearly
-  def calculate_monthly_or_yearly_interest_rate(rate)
-    calculated_interest_rate = rate
-
-    loop do
-      puts prompt 'enter_interest_rate_timing'
-      interest_rate_timing = gets.chomp.downcase.strip
-      if ['y', 'a', 'm'].include?(interest_rate_timing)
-        if interest_rate_timing.start_with?('m')
-          calculated_interest_rate = rate / 12
-        end
-        break
-      else
-        puts prompt 'invalid_input'
-      end
-    end
-    calculated_interest_rate
-  end
-
-  # Get the interest rate. Used by
-  def get_interest_rate
-    interest_rate = ''
-
-    loop do
-      puts prompt 'enter_interest_rate'
-      interest_rate = gets.chomp.strip
-      if valid_number_entry? interest_rate
-        interest_rate =
-          calculate_monthly_or_yearly_interest_rate(interest_rate.to_f / 100)
-        break
-      else
-        puts prompt 'invalid_number'
-      end
-    end
-    interest_rate
-  end
-  interest_rate = get_interest_rate
-
-  # Get whether loan duration is in months or years, and if it is in years,
-  # convert it into months
-  def calculate_loan_duration(time)
-    calculated_loan_duration = time
-
-    loop do
-      puts prompt 'enter_loan_duration_timing'
-      loan_duration_timing = gets.chomp.downcase.strip
-      if ['m', 'y', 'a'].include?(loan_duration_timing)
-        if ['y', 'a'].include?(loan_duration_timing)
-          calculated_loan_duration = time * 12
-        end
-        break
-      else
-        puts prompt 'invalid_input'
-      end
-    end
-    calculated_loan_duration
-  end
-
-  # Get loan duration. Used by calculate_loan_duration
-  # to determine loan_duration
-  def get_loan_duration
-    loan_duration = ''
-
-    loop do
-      puts prompt 'enter_loan_duration'
-      loan_duration = gets.chomp.strip
-      if valid_number_entry? loan_duration
-        loan_duration = calculate_loan_duration(loan_duration.to_i)
-        break
-      else
-        puts prompt 'invalid_number'
-      end
-    end
-    loan_duration
-  end
-  loan_duration = get_loan_duration
-
-  # Uses loan_amount, interest_rate, and loan_duration to calculate
-  # monthly payment
-  def calculate_monthly_payment(amount, rate, duration)
-    amount * (rate / (1 - (1 + rate)**(-duration)))
-  end
-
-  monthly_payment =
-    calculate_monthly_payment(loan_amount, interest_rate,
-                              loan_duration).round(2)
-  
-  puts format(prompt('monthly_payment_amount'), amount: monthly_payment)
-
-  # Asks user if they would like to make another calculation.
-  # if yes, the app loops back to beginning of logic loop.
-  # If no, breaks out of loop
-  def run_again?
-    loop do
-      puts prompt 'another_calculation'
-      run_again = gets.chomp.downcase.strip
-
-      case run_again
-      when 'y', 's' then break true
-      when 'n' then break false
-      else prompt 'invalid_input'
-      end
+  loop do
+    puts prompt 'enter_amount'
+    loan_amount = gets.chomp.strip
+    if valid_number_entry? loan_amount
+      loan_amount = loan_amount.to_f
+      break
+    else
+      puts prompt 'invalid_number'
     end
   end
 
-  break unless run_again?
+  loan_amount
+end
+
+# Get the interest rate amount
+def get_interest_rate
+  interest_rate = ''
+
+  loop do
+    puts prompt 'enter_interest_rate'
+    interest_rate = gets.chomp.strip
+    if valid_number_entry? interest_rate
+      break
+    else
+      puts prompt 'invalid_number'
+    end
+  end
+
+  interest_rate
+end
+
+# Get whether interest rate is monthly or yearly
+def get_interest_rate_timing
+  timing = ''
+
+  loop do
+    puts prompt 'enter_interest_rate_timing'
+    timing = gets.chomp.downcase.strip
+    if ['y', 'a', 'm'].include?(timing)
+      break
+    else
+      puts prompt 'invalid_input'
+    end
+  end
+
+  timing
+end
+
+# Calculate the final interest rate
+def calculate_interest_rate(rate, timing)
+  # Convert rate to decimal (ex. 5 to .05)
+  rate = rate.to_f / 100
+  if timing == 'm'
+    rate /= 12
+  end
+
+  rate
+end
+
+# Get loan duration
+def get_loan_duration
+  loan_duration = ''
+
+  loop do
+    puts prompt 'enter_loan_duration'
+    loan_duration = gets.chomp.strip
+    if valid_number_entry? loan_duration
+      break
+    else
+      puts prompt 'invalid_number'
+    end
+  end
+
+  loan_duration
+end
+
+# Get whether loan duration is in months or years
+def get_loan_duration_timing
+  timing = ''
+
+  loop do
+    puts prompt 'enter_loan_duration_timing'
+    timing = gets.chomp.downcase.strip
+    if %w(y m a).include?(timing)
+      break
+    else
+      puts prompt 'invalid_input'
+    end
+  end
+
+  timing
+end
+
+# Calculate final loan duration (in months)
+def calculate_loan_duration(duration, timing)
+  if timing.start_with? 'y', 'a'
+    duration = duration.to_i * 12
+  end
+
+  duration
+end
+
+# Uses loan_amount, interest_rate, and loan_duration to calculate
+# monthly payment
+def calculate_monthly_payment(amount, rate, duration)
+  amount * (rate / (1 - (1 + rate)**(-duration)))
+end
+
+# Asks user if they would like to make another calculation.
+# if yes, the app loops back to beginning of logic loop.
+# If no, breaks out of loop
+def run_again?
+  loop do
+    puts prompt 'another_calculation'
+    run_again = gets.chomp.downcase.strip
+
+    case run_again
+    when 'y', 's' then break true
+    when 'n' then break false
+    else prompt 'invalid_input'
+    end
+  end
 end
 
 # Thanks the user and says goodbye
@@ -205,5 +204,33 @@ def goodbye(user_name)
   puts format(prompt('thank_you'), name: user_name)
   puts prompt 'goodbye'
 end
+
+# PROMPT_FILE is referenced directly prior to determining desired output
+# language
+puts "=> #{PROMPT_FILE['welcome']}"
+
+get_lang
+
+users_name = get_users_name
+
+# Main logic loop
+loop do
+  clear_screen
+
+  loan_amount = get_loan_amount
+
+  interest_rate = calculate_interest_rate(get_interest_rate, get_interest_rate_timing)
+
+  loan_duration = calculate_loan_duration(get_loan_duration, get_loan_duration_timing)
+
+  monthly_payment =
+    calculate_monthly_payment(loan_amount, interest_rate,
+                              loan_duration).round(2)
+  
+  puts format(prompt('monthly_payment_amount'), amount: monthly_payment)
+
+  break unless run_again?
+end
+
 clear_screen
 goodbye users_name
